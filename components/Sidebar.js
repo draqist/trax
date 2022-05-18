3dcc33e78b8dba72e0dd38051420c6a963cdade2
Draqode <Boomjay008@gmail.com>
2022-05-18 23:48:37 +0100

    feat: tablet responsive

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -53,32 +53,32 @@ const Sidebar = () => {
       <Box display={['none','none','none','block','block']} w='100%' h='532px' mt='80px'>
         <List w='100%' color='#65676D' >
           <NextLink passHref href='/'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} _hover={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={MdOutlineSpaceDashboard} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
           <NextLink passHref href='/projects'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={IoBriefcaseOutline} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
           <NextLink passHref href='tasks'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={AiOutlineProfile} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
           <NextLink passHref href='calendar'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={IoCalendarOutline} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
           <NextLink passHref href='mgmt'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={AiOutlinePieChart} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
           <NextLink passHref href='settings'>
-            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
+            <ListItem w='100%'  textTransform='uppercase' fontWeight='600' fontSize='18px' py='36px' _active={{bg:'#FFF4EE', color:'#FD7B38', borderRight: '6px solid #FD7B38'}} px='40px' _hover={{bg:'#FFF4EE', color:'#FD7B38',borderRight: '6px solid #FD7B38'}}>
               <ListIcon as={IoSettingsOutline} mr='20px'  fontSize='36px' />
             </ListItem>
           </NextLink>
